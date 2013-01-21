9eda078c3aedb82d845c42521cb2984a8a26883e
Mark Soper <masoper@gmail.com>
2013-01-21 08:51:14 -0500

    worked a bit on an arc approach to strokes, will probably do bezier curve instead

--- src/stroke.js
+++ src/stroke.js
@@ -11,11 +11,12 @@ var GEN;
 
   var Stroke = (function () {
 
-    function Stroke(startX, startY, endX, endY, options) {
+    function Stroke(startX, startY, endX, endY, curvature, options) {
       this.startX = startX;
       this.startY = startY;
       this.endX = endX;
       this.endY = endY;
+      this.curvature = curvature;
       this.options = options || {};
     }
 
@@ -25,11 +26,13 @@ var GEN;
       this.color = this.options.color || new GEN.Color(context.strokeStyle);
       this.shadeRange = this.options.shadeRange || 0.5;
       context.lineCap = "round";
-      var color;
+      var color, radius;
       for (var i=0; i<reps; i++) {
         context.StrokeStyle = (this.color.getRandomShade(0.8)).rgba();
         context.lineWidth = Math.floor( Math.max(1, (this.lineWidth/5 - this.lineWidth/8) * Math.random() + this.lineWidth/8 ) );
+        radius = Math.sqrt( Math.pow( ( this.startX - this.endX ), 2 ) + Math.pow( ( this.startY - this.endY ), 2 ) ) / ( 2 * Math.sin( Math.PI * this.curvature / 2 ) );
         
+
 
         radius = Math.floor ( this.radius + ( (this.lineWidth - context.lineWidth) * Math.random() - (this.lineWidth / 2 - context.lineWidth / 2) ) );
         mpVar = (this.radius - radius) / 4 + (this.lineWidth - context.lineWidth) / 4;
