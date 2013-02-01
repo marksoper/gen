ddbb7a5bfafe3fab1b2c93abbd501204f0227bec
Mark Soper <masoper@gmail.com>
2013-02-01 13:23:10 -0500

    publish latest demos from master branch

--- utils/build.sh
+++ utils/build.sh
@@ -3,7 +3,7 @@
 cd /mark/genpages
 cp -r ../gen/demos/ demos
 cp ../gen/build/gen.js ./build/
-find demos -name "*.html" -type f -exec sed -i.bak 's|<\/head>|<script type="text\/javascript" src="\/js\/ga.js"><\/script><\/head>|g' {} \;
+find demos -name "*.html" -type f -exec sed -i.bak 's|<\/head>|<script type="text\/javascript" src="\/js\/gaq.js"><\/script><\/head>|g' {} \;
 git add -A
 git commit -m 'publish latest demos from master branch'
 git push origin gh-pages
